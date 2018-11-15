854bb469e5ed1293e1867f43b625ee3236eb5ebf
Daniel Tyebkhan <19tyebkhand@flintridgeprep.org>
2018-11-14 16:30:30 -0800

    finished first bonus

--- Cat.java
+++ Cat.java
@@ -1,23 +1,29 @@
 
 public class Cat {
-private String name;
-private double weight; 
-public void setName(String aname){
-    name = aname;
-}
-public void setWeight(double aweight){
-    weight = aweight;
-}
+    private String name;
+    private double weight; 
+    public void setName(String aname){
+        name = aname;
+    }
 
-public String getName(){
-    return name;
-}
+    public void setWeight(double aweight){
+        weight = aweight;
+    }
 
-public double getWeight(){
-    return weight;
-}
+    public String getName(){
+        return name;
+    }
 
-public void meow(){
-System.out.println("Meow!");
-}
+    public double getWeight(){
+        return weight;
+    }
+
+    public void meow(){
+        double rng = Math.random()*10;
+        if(rng<=2.5){
+            System.out.println("Grrrrr");
+        }else{
+            System.out.println("Meow!");
+        }
+    }
 }

f5b884b8bcafa1d4dc304f166835f6880e77d9fe
Daniel Tyebkhan <19tyebkhand@flintridgeprep.org>
2018-11-14 16:00:16 -0800

    finished cat class and started testcatlist

--- Cat.java
+++ Cat.java
@@ -1,3 +1,23 @@
 
 public class Cat {
+private String name;
+private double weight; 
+public void setName(String aname){
+    name = aname;
+}
+public void setWeight(double aweight){
+    weight = aweight;
+}
+
+public String getName(){
+    return name;
+}
+
+public double getWeight(){
+    return weight;
+}
+
+public static void meow(){
+System.out.println("Meow!");
+}
 }
---
+++ TestCatList.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class TestCatList{
+    public static void main(String[] args){
+        ArrayList <Cat> CatList = new ArrayList <Cat>();
+        
+    }
+}

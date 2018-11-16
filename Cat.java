3d986d6e710ec26eaac58bef2711eacf6b71d0c2
Daniel Tyebkhan <19tyebkhand@flintridgeprep.org>
2018-11-16 09:17:16 -0800

    added comments

--- Cat.java
+++ Cat.java
@@ -1,30 +1,30 @@
-import java.util.*;
+import java.util.*; //imports java utilities
 
 public class Cat {
-    private String name;
-    private double weight; 
-    public void setName(){
+    private String name; //creates a private variable to store the cats' names
+    private double weight; //creates a private variable to store the cats' weights
+    public void setName(){//creates scanner variable and gets user input for the cats' names
         Scanner input = new Scanner(System.in);
         name = input.nextLine().toString();
     }
 
-    public void setWeight(double aweight){
+    public void setWeight(double aweight){//sets the weight to the inputted parameter
         weight = aweight;
     }
 
-    public String getName(){
+    public String getName(){//returns the cat's name
         return name;
     }
     
-    public double getWeight(){
+    public double getWeight(){//returns the cat's wieght
         return weight;
     }
 
-    public void meow(){
-        double rng = Math.random();
-        if(rng<=.25){
+    public void meow(){//makes the cat meow
+        double rng = Math.random();//creates a variable that stores a random number between 0 and 1
+        if(rng<=.25){//gives a 25% chance to say grrr
             System.out.println("Grrrrr");
-        }else{
+        }else{//cat says meow
             System.out.println("Meow!");
         }
     }
--- TestCatList.java
+++ TestCatList.java
@@ -1,17 +1,17 @@
-import java.util.*;
+import java.util.*; //imports java utilities
 public class TestCatList{
     public static void main(String[] args){
-        ArrayList <Cat> CatList = new ArrayList <Cat>();
-        for(int i = 1; i<5; i++){
+        ArrayList <Cat> CatList = new ArrayList <Cat>(); //creates an arraylist of cats
+        for(int i = 1; i<5; i++){ //for loop to populate the arraylist with 4 cats
             CatList.add(new Cat());
         }
-        for(int i = 0; i<CatList.size(); i++){
+        for(int i = 0; i<CatList.size(); i++){//for loop that uses the setName method to get user input to name the cats
             System.out.println("Enter the name of cat " + (i+1));
             CatList.get(i).setName();
         }
-        for(Cat c:CatList){
+        for(Cat c:CatList){//modified for loop that uses the setWeight method to randomly assign each cat a weight then prints each cat's name and weight and has them meow with the meow method
             c.setWeight(Math.random()*10+5);
-            System.out.println(c.getWeight() + " " + c.getName());
+            System.out.println(c.getName() + " weighs " + c.getWeight());
             c.meow();
         }
     }
